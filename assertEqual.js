91186474b21376d391b58cfd9b30a6e618a4a448
stevegators <dsteven1@gmail.com>
2019-10-14 17:28:24 -0400

    created working code for assert Equal

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😊 😊 😊 Assertion Passed:" + actual + "===" + expected);
+  } else if (actual !== expected) {
+    console.log("😢 😢 😢 Assertion Failed:" + actual + "!==" + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// If the values match, it should print (console.log) the following: Assertion Passed: [actual] === [expected] (but with the values filled in)
+// Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
