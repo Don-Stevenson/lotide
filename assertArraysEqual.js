6132a1b0d91a93457de6975022191ff2ae8eaf81
Don-Stevenson <dsteven1@gmail.com>
2019-10-15 23:29:36 +0000

    working code for assert array is equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+
+const eqArrays = function (A,B){
+  if (A.length !== B.length){
+    return false
+  }
+  for (let i = 0; i < B.length ; i++){
+    if(A[i] !== B[i]){
+      return false
+    } 
+  }
+  return true
+}
+
+const assertArraysEqual = function(A, B) {
+  if (eqArrays(A,B)) {
+    console.log(`😊 😊 😊 Assertion Passed: [${A}] === [${B}]`);
+  } else {
+    console.log(`😢 😢 😢 Assertion Failed: [${A}] !== [${B}]`);
+  }
+}
+assertArraysEqual([1,2,3],['A','B','C']);
+
